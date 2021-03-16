fd6002f152181cd3622ec6a40294b461cf9d29df
DrielyGuerreiro <68483756+DrielyGuerreiro@users.noreply.github.com>
2021-03-16 19:47:37 -0300

    Add files via upload

---
+++ IMC.php
@@ -0,0 +1,37 @@
+<form method="post"action="calcular.php">
+
+Peso: <input type="text" size="20" name="peso">
+
+Altura: <input type="text" size="20" name="altura">
+
+<input type="submit" value="Calcular">
+
+</form>
+
+<?php
+
+$peso = $_POST['peso'];
+$altura = $_POST['altura'];
+$altura = bcpow($altura, 2, 2);
+$massa = $peso / $altura;
+$massa = round($massa);
+
+if($massa < 20) {
+
+$mensagem = "Você está abaixo do ideal.";
+
+}elseif(($massa > 20) and ($massa < 25)) {
+
+$mesagem = "Você está no peso ideal.";
+
+}else{
+
+$mesagem = "Você está acima do ideal.";
+
+}
+
+echo "Sua massa corporal é: <b>$massa</b> <br>";
+
+echo "Estado atual: <b>$mensagem</b>"
+
+?>
